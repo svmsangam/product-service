f59099d93c9881f06651da9b1d72ed488b3c029d
subham <sbmsangam786@gmail.com>
2025-11-14 09:37:06 +0545

    Setup integration test for product creation

--- src/test/java/com/webkart/microservice/product/ProductServiceApplicationTests.java
+++ src/test/java/com/webkart/microservice/product/ProductServiceApplicationTests.java
@@ -1,15 +1,42 @@
 package com.webkart.microservice.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private Integer port;
 
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void testCreateProduct() {
+        String requestBody = """
+                {
+                     "name":"Samsung Fold",
+                     "description":"A foldable phone from Samsung",
+                     "price":147580.00
+                 }
+                """;
+        RestAssured.given().contentType("application/json").body(requestBody)
+                .when().post("/api/product")
+                .then().statusCode(201).body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("Samsung Fold"))
+                .body("description", Matchers.equalTo("A foldable phone from Samsung"))
+                .body("price", Matchers.equalTo(147580.00f));
+        ;
+    }
 }
