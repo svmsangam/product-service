ac6326d0690fc4d5c785904f41eb5a154e8726d0
subham <sbmsangam786@gmail.com>
2025-11-30 19:32:58 +0545

    OpenAPI documentation setup

---
+++ src/main/java/com/webkart/microservice/product/config/OpenAPIConfiguration.java
@@ -0,0 +1,19 @@
+package com.webkart.microservice.product.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfiguration {
+    @Bean
+    public OpenAPI productServiceOpenAPI() {
+        return new OpenAPI().info(new Info().title("Product Service")
+                .description("REST API documentation for Product Service")
+                .version("1.0")
+                .license(new License().name("Apache 2.0")));
+
+    }
+}
